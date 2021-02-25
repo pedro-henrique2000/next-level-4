465114918574c6af22f035484de76c89c1efd5f2
phb2000 <p.becker2000@hotmail.com>
2021-02-24 22:58:24 -0300

    second day complete

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,14 @@
+import styles from '../styles/components/CompletedChallenges.module.css'
+
+export function CompletedChallenges() {
+    return(
+        <div className={styles.challengesContainer}>
+            <span>
+                Desafios Completos    
+            </span>
+            <span>
+                5
+            </span>
+        </div>
+    )
+}
